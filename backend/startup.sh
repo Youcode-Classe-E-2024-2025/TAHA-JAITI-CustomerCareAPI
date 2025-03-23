65c7d5acdbd49287cc9f6f32970b44238d79f572
taha jaiti <kyojin.rar@gmail.com>
2025-03-23 15:42:15 +0000

    FIX: fixed the issuees with docker concerning the permissions

--- backend/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chown -R www-data:www-data /var/www/html/storage
-chown -R www-data:www-data /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage
-chmod -R 775 /var/www/html/bootstrap/cache
-
-exec php-fpm
