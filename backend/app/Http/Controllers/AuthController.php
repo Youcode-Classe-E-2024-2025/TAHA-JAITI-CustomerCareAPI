c96a1a0c9347c70b2d941a31bf0723c731413ade
taha jaiti <kyojin.rar@gmail.com>
2025-03-23 16:16:04 +0000

    FEAT: setup the api routes and registerd them in the app bootstrap

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Res;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-    //
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+
+    public function register(RegisterRequest $request)
+    {
+        $res = $this->authService->register($request);
+
+        return $res ? Res::success($res, 'User Register Successfully', 201) : Res::error("Failed to register");
+    }
+
 }
