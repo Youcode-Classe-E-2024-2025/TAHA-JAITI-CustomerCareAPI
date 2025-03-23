930199fed80fca0aa201b44f588fa099b45ff900
taha jaiti <kyojin.rar@gmail.com>
2025-03-23 16:33:56 +0000

    FEAT: edited the users table to include role, created the tickets table with its model

--- backend/database/migrations/2025_03_23_163202_create_tickets_table.php
+++ backend/database/migrations/2025_03_23_163202_create_tickets_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['agent', 'user'])->default('user');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['open', 'in_progress', 'closed'])->default('open');
+            $table->foreignId('assigned_to')->nullable()->constrained('users')->nullOnDelete();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
