4b5afcc2a5d5ef23eefedac1f191a64adf1fa1e1
taha jaiti <kyojin.rar@gmail.com>
2025-03-23 16:45:57 +0000

    FEAT: registered the ticket middleware, added the route

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'jwt' => \App\Http\Middleware\AuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["prefix"=> "auth"], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
+});
+
+
+Route::middleware(['jwt'])->group(function () {
+    Route::post('/ticket', [TicketController::class, 'create']);
 });
