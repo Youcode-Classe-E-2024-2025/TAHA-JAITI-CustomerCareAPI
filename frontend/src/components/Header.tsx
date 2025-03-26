57a7fa8755994b27b5faea450a88d88f58237058
taha jaiti <kyojin.rar@gmail.com>
2025-03-26 11:34:46 +0000

    FEAT: implemnted a simple header, changed the base font to poppins

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Ticket } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="bg-reen-200 py-4 px-6 flex justify-between items-center shadow-md">
+      <h1 className="text-2xl font-bold flex items-center gap-2">
+        <Ticket />
+        DimaLeek
+      </h1>
+      <nav>
+        <ul className="flex space-x-6">
+          <li>
+            <a href="#" className="text-reen-400 hover:text-reen-300 transition">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-reen-400 hover:text-reen-300 transition">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-reen-400 hover:text-reen-300 transition">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
